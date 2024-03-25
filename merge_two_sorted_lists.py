56ea073a4c29431e7134d257d0f4e91371b43c63
mxrtydluffy <marty.marinda@students.dominican.edu>
2024-03-24 22:32:01 -0700

    [COMPLETE] Problems

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -11,3 +11,35 @@
 # Example 1
 # Input: list1 = [1,2,4], list2 = [1,3,4]
 # Output: [1,1,2,3,4,4]
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # If linked 1 doesn't exist return linked list 2.
+        if not l1: return l2
+        # If linked 2 doesn't exist retrun linked list 1.
+        if not l2: return l1
+
+        # Ensure to return the head later and assing a ListNode inorder to create new linked list.
+        current = dummy = ListNode(0)
+
+        # Want to check if linked 1 value is less than l2 or L2 value is less than l1 value 
+        while l1 and l2:
+            # if l1 value is less than l2 value add to the current next node.
+            if l1.val < l2.val:
+                current.next = l1
+                #Then increment li pointer to the next.
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            # make sure third pointer is incremented
+            current = current.next
+
+        # Once finished, need to make sure to add the rest of l1 and l2 dependeing on which one remains.
+        if l1: current.next = l1
+        if l2: current.next = l2
+
+        # Return head by using dummy pointer. Needs to point to next because its pointing to a point
+        # that doesn't exist on this linked list that is created since its an empty node. The one after
+        # that is the head.
+        return dummy.next
--- valid_parentheses.py
+++ valid_parentheses.py
@@ -23,4 +23,27 @@
 # Example 3
 
 # Input: s = "(]"
-# Output: true
+# Output: false
+
+def isValid(s):
+    # To start off create a pair of opening and closing parrenthesis inside a dictionary because need to access
+    # the characters later and assign it to a variable.
+    opcl = dict(('()', '[]', '{}'))
+    # Create list data structure and assign it to a variable.
+    stack = []
+    # Loop through each charater in the input string
+    for idx in s:
+        # If the open parentheses are present in the index, append it to stack...
+        if idx in '([{':
+            stack.append(idx)
+        # If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not.
+        # If not, we need to return false...
+        elif len(stack) == 0 or idx != opcl[stack.pop()]:
+            return False
+    # lastly, we check if the stack is empty or not...
+    # If the stack is empty, it means every opened parenthesis is being closed and we can return true, otherwise we return false.
+    return len(stack) == 0
+
+print(isValid("()"))
+print(isValid("()[]{}"))
+print(isValid("(]"))
