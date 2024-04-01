ce4ccafe03b9c5e496794b63347e568cf7a04b43
mxrtydluffy <marty.marinda@students.dominican.edu>
2024-03-31 21:51:46 -0700

    [ADD] Questions

---
+++ palindrome_number.py
@@ -0,0 +1,27 @@
+# Question
+
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+#_____________________________________________________________________________________________________________________________________
+
+# Example #1
+
+# Input x = 121
+# Output: True
+# Explanation: 121 reads as 121 from left to right and from right to left
+
+# _____________________________________________________________________________________________________________________________________
+
+# Example #2
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+# _____________________________________________________________________________________________________________________________________
+
+# Example #3
+
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
